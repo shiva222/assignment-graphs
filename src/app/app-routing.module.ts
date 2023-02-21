a534212864685b7a1c394c8b1ad3d3edadbfce0e
Shiva Kumar Nayini <shiva.n@comakeit.com>
2023-02-21 16:41:01 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraphLayoutComponent } from './graph-layout/graph-layout.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component: GraphLayoutComponent},
+
+  {path:'graph', component: GraphLayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ApiHttpService } from "../app/core/services/ApiHttpService";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphLayoutComponent } from './graph-layout/graph-layout.component';
+import { NgChartsModule } from 'ng2-charts';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiHttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
